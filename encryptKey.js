abbc6f72abac95da4087265eb78a2f0128c9ed64
root <root@zhj.>
2025-04-04 21:29:15 +0800

    initial commit

---
+++ encryptKey.js
@@ -0,0 +1,19 @@
+const ethers = require("ethers");
+const fs = require("fs");
+require("dotenv").config();
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const encryptedJSONKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+  console.log(encryptedJSONKey);
+  fs.writeFileSync("./.encryptedKey.json", encryptedJSONKey);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
