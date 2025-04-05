610d639f4446452be4998b1d27cf072b085e1280
codersjj <shanewestlife@outlook.com>
2025-04-05 21:33:58 +0800

    chore: migrate to ES modules and update import statements

--- encryptKey.js
+++ encryptKey.js
@@ -1,19 +1,22 @@
-const ethers = require("ethers");
-const fs = require("fs");
-require("dotenv").config();
+// const ethers = require("ethers");
+// const fs = require("fs");
+// require("dotenv").config();
+import { ethers } from "ethers"
+import fs from "fs"
+import "dotenv/config"
 
 async function main() {
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
   const encryptedJSONKey = await wallet.encrypt(
-    process.env.PRIVATE_KEY_PASSWORD
-  );
-  console.log(encryptedJSONKey);
-  fs.writeFileSync("./.encryptedKey.json", encryptedJSONKey);
+    process.env.PRIVATE_KEY_PASSWORD,
+  )
+  console.log(encryptedJSONKey)
+  fs.writeFileSync("./.encryptedKey.json", encryptedJSONKey)
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
